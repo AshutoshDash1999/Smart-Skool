b290de5754677ad661b585e200a1929ef5cc6837
Ashutosh Dash <ashu.vampire13@gmail.com>
2020-09-06 14:16:38 +0530

    Add files via upload

---
+++ student-teachers-panel/coding-monks-teacher/assets/js/class.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    elem = $(".elem");
+    elem.mouseup(function(event) {
+        if ($(this).children("input").length == 0) {
+            var old = $(this).html();
+            $(this).empty();
+            var input = $('<input type=text>');
+            input.val(old);
+            $(this).append(input);      
+            input.select();
+            input.blur(function() {
+                var value = $(this).val();
+                if (value !== "") {
+                    $(this).parent().html($(this).val());
+                } else {
+                    $(this).parent().html("Text");
+                }
+            });
+        }
+    });
+    });
